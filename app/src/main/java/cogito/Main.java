8b65180070f964c520f13d47e0f7e17610c58fc9
MORON USON guillermo <guillermo.moron@laposte.net>
2025-01-25 17:37:05 +0100

    Setup: sets up project * Adds a .gitignore * Sets up gradle

---
+++ app/src/main/java/cogito/Main.java
@@ -0,0 +1,16 @@
+package cogito;
+
+/**
+ * Entry class of the program.
+ */
+public class Main {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args The arguments given to the program.
+     */
+    public static void main(String[] args) {
+
+    }
+}
